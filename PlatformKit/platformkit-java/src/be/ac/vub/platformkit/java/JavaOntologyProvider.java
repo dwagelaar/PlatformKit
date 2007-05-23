1540b310b6a78b1dab4286e36576abbc60b6ebcb
dwagelaa <dwagelaa@5ad673d7-9de5-0310-9841-9683b5787651>
2007-05-23 15:14:31 +0000

    New cross-plugin base ontology provisioning system

---
+++ PlatformKit/platformkit-java/src/be/ac/vub/platformkit/java/JavaOntologyProvider.java
@@ -0,0 +1,32 @@
+package be.ac.vub.platformkit.java;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Logger;
+
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
+
+import be.ac.vub.platformkit.kb.IOntologyProvider;
+import be.ac.vub.platformkit.kb.Ontologies;
+
+public class JavaOntologyProvider implements IOntologyProvider {
+
+	private static final String[] ontologies = new String[] {
+		"ontology/codamos_2007_01/jdk-1_1-api.owl",
+		"ontology/codamos_2007_01/j2se-1_2-api.owl"
+	};
+	
+	protected static Bundle bundle = Platform.getBundle("be.ac.vub.platformkit.java");
+    protected static Logger logger = Logger.getLogger(Ontologies.LOGGER);
+
+	public InputStream[] getOntologies() throws IOException {
+		InputStream[] streams = new InputStream[ontologies.length];
+		for (int i = 0; i < ontologies.length; i++) {
+			streams[i] = bundle.getResource(ontologies[i]).openStream();
+			logger.fine("Providing ontology " + ontologies[i] + " as " + streams[i]);
+		}
+		return streams;
+	}
+
+}
