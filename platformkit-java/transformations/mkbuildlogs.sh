45eaeaaaffcf55b29c66d8526a3be76a19e0a22d
dwagelaa <dwagelaa@5ad673d7-9de5-0310-9841-9683b5787651>
2007-06-28 07:15:21 +0000

    Added J2ME PP 1.1 and J2ME MIDP 2.0 to the build and ran preliminary ontology transformations

--- platformkit-java/transformations/mkbuildlogs.sh
+++ platformkit-java/transformations/mkbuildlogs.sh
@@ -2,7 +2,9 @@
 
 cp build.log build.log.txt
 grep "\[j2me-midp-1_0\]" build.log.txt > build.log.j2me-midp-1_0.txt
+grep "\[j2me-midp-2_0\]" build.log.txt > build.log.j2me-midp-2_0.txt
 grep "\[j2me-pp-1_0\]" build.log.txt > build.log.j2me-pp-1_0.txt
+grep "\[j2me-pp-1_1\]" build.log.txt > build.log.j2me-pp-1_1.txt
 grep "\[personaljava-1_1\]" build.log.txt > build.log.personaljava-1_1.txt
 grep "\[jdk-1_1\]" build.log.txt > build.log.jdk-1_1.txt
 grep "\[j2se-1_2\]" build.log.txt > build.log.j2se-1_2.txt
