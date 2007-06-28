ce9d77ebf1e6267d34dc7e1dcbb9a03c831e0d73
dwagelaa <dwagelaa@5ad673d7-9de5-0310-9841-9683b5787651>
2007-06-28 12:46:38 +0000

    Updated existing examples and added new generic platform examples for J2ME PP 1.0, Personal Java 1.1 and J2ME MIDP 1.0

--- platformkit-examples/src/be/ac/vub/platformkit/examples/ExamplesOntologyProvider.java
+++ platformkit-examples/src/be/ac/vub/platformkit/examples/ExamplesOntologyProvider.java
@@ -21,6 +21,10 @@ public class ExamplesOntologyProvider implements INamedOntologyProvider {
         "codamos_2007_01/Generic/JDK1.3PC.owl",
         "codamos_2007_01/Generic/JDK1.4PC.owl",
         "codamos_2007_01/Generic/JDK1.5PC.owl",
+        "codamos_2007_01/Generic/JDK1.6PC.owl",
+        "codamos_2007_01/Generic/PersonalJava1.1PocketPC.owl",
+        "codamos_2007_01/Generic/J2MEPP1.0PocketPC.owl",
+        "codamos_2007_01/Generic/J2MEMIDP1.0Phone.owl",
         "codamos_2007_01/Sharp/ZaurusSL-C1000PP.owl",
         "codamos_2007_01/Sharp/ZaurusSL-C1000Jeode.owl",
         "codamos_2007_01/Siemens/CX70v.owl"
@@ -32,6 +36,10 @@ public class ExamplesOntologyProvider implements INamedOntologyProvider {
         "JDK 1.3 PC",
         "JDK 1.4 PC",
         "JDK 1.5 PC",
+        "JDK 1.6 PC",
+        "Microsoft PocketPC with Personal Java 1.1",
+        "Microsoft PocketPC with J2ME PP 1.0",
+        "Mobile phone with J2ME MIDP 1.0",
         "Sharp Zaurus SL-C1000 PDA with J2ME PP 1.0",
         "Sharp Zaurus SL-C1000 PDA with Jeode Personal Java 1.1",
         "Siemens CX70v mobile phone with J2ME MIDP 1.0"
