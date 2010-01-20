27637168273f8f66b11a2f3774229a586b785945
dwagelaa <dwagelaa@5ad673d7-9de5-0310-9841-9683b5787651>
2010-01-20 07:43:36 +0000

    Added SWT 3.5 API

--- platformkit-java/transformations/swt/mkbuildlogs.sh
+++ platformkit-java/transformations/swt/mkbuildlogs.sh
@@ -6,3 +6,4 @@ grep "\[swt-3_1\]" build.log.txt > build.log.swt-3_1.txt
 grep "\[swt-3_2\]" build.log.txt > build.log.swt-3_2.txt
 grep "\[swt-3_3\]" build.log.txt > build.log.swt-3_3.txt
 grep "\[swt-3_4\]" build.log.txt > build.log.swt-3_4.txt
+grep "\[swt-3_5\]" build.log.txt > build.log.swt-3_5.txt
