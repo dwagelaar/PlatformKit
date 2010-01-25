2c9582f9db66b6a6f4820ff6fe816525ea970b99
dwagelaa <dwagelaa@5ad673d7-9de5-0310-9841-9683b5787651>
2010-01-25 14:00:43 +0000

    Progress monitor functionality moved to ProgressMonitorJob.

---
+++ platformkit-java/src/be/ac/vub/platformkit/java/popup/actions/SelectionAction.java
@@ -0,0 +1,32 @@
+package be.ac.vub.platformkit.java.popup.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IActionDelegate;
+
+/**
+ * General superclass for popup actions with a selection.
+ * @author Dennis Wagelaar <dennis.wagelaar@vub.ac.be>
+ */
+public abstract class SelectionAction implements IActionDelegate {
+
+	protected ISelection selection;
+	protected IAction action;
+
+	/**
+	 * Creates a new {@link SelectionAction}.
+	 */
+	public SelectionAction() {
+		super();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+	    this.selection = selection;
+	    this.action = action;
+	}
+
+}
