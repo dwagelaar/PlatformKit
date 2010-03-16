8dc26f8c69e65429d5e8d037cedae3f23d454b59
dwagelaa <dwagelaa@5ad673d7-9de5-0310-9841-9683b5787651>
2010-03-16 11:39:19 +0000

    Java platform ontologies generated from new API models.

--- platformkit-java/build-ontology/jre/mkbuildlogs.sh
+++ platformkit-java/build-ontology/jre/mkbuildlogs.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 cp build.log build.log.txt
-grep "\[j2me-midp-1_0\]" build.log.txt > build.log.j2me-midp-1_0.txt
-grep "\[j2me-midp-2_0\]" build.log.txt > build.log.j2me-midp-2_0.txt
-grep "\[j2me-pp-1_0\]" build.log.txt > build.log.j2me-pp-1_0.txt
-grep "\[j2me-pp-1_1\]" build.log.txt > build.log.j2me-pp-1_1.txt
-grep "\[personaljava-1_1\]" build.log.txt > build.log.personaljava-1_1.txt
-grep "\[jdk-1_1\]" build.log.txt > build.log.jdk-1_1.txt
-grep "\[j2se-1_2\]" build.log.txt > build.log.j2se-1_2.txt
-grep "\[j2se-1_3\]" build.log.txt > build.log.j2se-1_3.txt
-grep "\[j2se-1_4\]" build.log.txt > build.log.j2se-1_4.txt
-grep "\[j2se-1_5\]" build.log.txt > build.log.j2se-1_5.txt
-grep "\[j2se-1_6\]" build.log.txt > build.log.j2se-1_6.txt
+grep "\[J2ME MIDP 1.0\]" build.log.txt > build.log.j2me-midp-1_0.txt
+grep "\[J2ME MIDP 2.0\]" build.log.txt > build.log.j2me-midp-2_0.txt
+grep "\[J2ME PP 1.0\]" build.log.txt > build.log.j2me-pp-1_0.txt
+grep "\[J2ME PP 1.1\]" build.log.txt > build.log.j2me-pp-1_1.txt
+grep "\[PersonalJava 1.1\]" build.log.txt > build.log.personaljava-1_1.txt
+grep "\[JDK 1.1\]" build.log.txt > build.log.jdk-1_1.txt
+grep "\[J2SE 1.2\]" build.log.txt > build.log.j2se-1_2.txt
+grep "\[J2SE 1.3\]" build.log.txt > build.log.j2se-1_3.txt
+grep "\[J2SE 1.4\]" build.log.txt > build.log.j2se-1_4.txt
+grep "\[J2SE 5.0\]" build.log.txt > build.log.j2se-1_5.txt
+grep "\[Java SE 6\]" build.log.txt > build.log.j2se-1_6.txt
--- platformkit-java/build-ontology/swt/mkbuildlogs.sh
+++ platformkit-java/build-ontology/swt/mkbuildlogs.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 cp build.log build.log.txt
-grep "\[swt-3_0\]" build.log.txt > build.log.swt-3_0.txt
-grep "\[swt-3_1\]" build.log.txt > build.log.swt-3_1.txt
-grep "\[swt-3_2\]" build.log.txt > build.log.swt-3_2.txt
-grep "\[swt-3_3\]" build.log.txt > build.log.swt-3_3.txt
-grep "\[swt-3_4\]" build.log.txt > build.log.swt-3_4.txt
-grep "\[swt-3_5\]" build.log.txt > build.log.swt-3_5.txt
+grep "\[SWT 3.0\]" build.log.txt > build.log.swt-3_0.txt
+grep "\[SWT 3.1\]" build.log.txt > build.log.swt-3_1.txt
+grep "\[SWT 3.2\]" build.log.txt > build.log.swt-3_2.txt
+grep "\[SWT 3.3\]" build.log.txt > build.log.swt-3_3.txt
+grep "\[SWT 3.4\]" build.log.txt > build.log.swt-3_4.txt
+grep "\[SWT 3.5\]" build.log.txt > build.log.swt-3_5.txt
